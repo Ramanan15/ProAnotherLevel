817fce774dbeefa92bbfecadd8af6ea52aa86480
Ramanan15 <billaramana15@gmail.com>
2017-08-04 12:32:53 +0530

    Update Pro51.java

--- Pro51.java
+++ Pro51.java
@@ -1,22 +1,15 @@
 import java.util.*;
-public class P 51 
-{
-public static void main(String[] args) 
-{
-	Scanner sc=new Scanner(System.in);
-	int row=1;
-	int column=5;
-
-	int result=0;
-	int r=row-1;
-
-	for(int i=0;i<column;i++) 
-{
-		result=result+r;
+public class Pro51 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int row = 1;
+		int column = 5;
+		int result = 0;
+		int r = row - 1;
+			for(int i = 0;i < column; i++) {
+				result = result+r;
+			}
+		result = result + (column - 1);
+		System.out.println("The mimimum number of cutting: "+result);
 	}
-	result=result+(column-1);
-
-	System.out.println("The mimimum number of cutting: "+result);
-}
-
 }
