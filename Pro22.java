6a9710af2df1d87d024f63c25c67ff31f9f6f5ff
Ramanan15 <billaramana15@gmail.com>
2017-08-04 12:26:26 +0530

    Update Pro22.java

--- Pro22.java
+++ Pro22.java
@@ -1,37 +1,22 @@
-public class P 22 
-{
-	public static void main(String[]args)
-{
-	 //Rectagle one edges(x1,y1) and(x2,y2)
-		//Rectagle one edges(x3,y3) and(x4,y4)
+public class Pro22 {
+	public static void main(String[]args) {
 		int x1=10;
-
-	 int x2=15;
-	 int x3=10;
-
-	 int x4=15;
-	 int y1=5;
-
-	 int y2=10;
-	 int y3=5;
-	 int y4=10;
-
-    int leftX = Math.max(x1, x3);   
-    int rightX = Math.min(x2, x4);
-
-    int botY = Math.max(y1, y3);
-    int topY = Math.min(y2, y4);
-
-    if (rightX >= leftX && topY >= botY)
-{
-       System.out.println("The triangle are overlaping");
-	
-}
-
-    else 
-{
-    	 System.out.println("The triangle not overlaping");
-    }
-}
-
+		int x2=15;
+		int x3=10;
+		int x4=15;
+		int y1=5;
+		int y2=10;
+		int y3=5;
+		int y4=10;
+		int leftX = Math.max(x1, x3);   
+		int rightX = Math.min(x2, x4);
+		int botY = Math.max(y1, y3);
+		int topY = Math.min(y2, y4);
+		    if (rightX >= leftX && topY >= botY) {
+		       System.out.println("The triangle are overlaping");
+		     }
+		    else {
+			 System.out.println("The triangle not overlaping");
+		    }
+	}
 }
