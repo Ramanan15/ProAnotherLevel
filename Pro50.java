09384a7026414cc68b4c11459d9ae89972747946
Ramanan15 <billaramana15@gmail.com>
2017-08-04 12:29:48 +0530

    Update Pro50.java

--- Pro50.java
+++ Pro50.java
@@ -1,29 +1,17 @@
 import java.util.*;
-public class P 50 
-{
-public static void main(String[] args)
- {
-	Scanner sc=new Scanner(System.in);
-
-	System.out.println("Enter the number of inputs");
-	int num=sc.nextInt();
-
-	int ip[]=new int[num];
-
-	System.out.println("Enter the input");
-
-	for(int i=0;i<num;i++)
-{
-	ip[i]=sc.nextInt();
-
-	
-}
-	for(int i=0;i<num;i++) 
-{
-		int len=String.valueOf(ip[i]).length();
-
-		System.out.println(len);
-		
+public class Pro50 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	 	System.out.println("Enter the number of inputs");
+		int num = sc.nextInt();
+		int ip[] = new int[num];
+		System.out.println("Enter the input");
+			for(int i = 0;i < num; i++) {
+				ip[i] = sc.nextInt();
+			}
+			for(int i = 0;i < num; i++) {
+				int len=String.valueOf(ip[i]).length();
+				System.out.println(len);
+			}
 	}
-}
 }
